e1c8a6d8199862cbc94fe679352faa8b3c43cf04
mahata <nori@mahata.net>
2013-01-11 02:53:35 -0800

    .

--- app/controllers/Index.scala
+++ app/controllers/Index.scala
@@ -7,4 +7,38 @@ object Index extends Controller {
   def index = Action {
     Ok(views.html.index("Hello, this is index!"))
   }
+
+  def test = Action {
+    implicit request => {
+      println(request)
+      println(request.host)
+      println(request.domain)
+      Ok(views.html.index("Hello, this is..., whatever :-)"))
+    }
+  }
+
+  def test2 = Action {
+    Ok("天才のそれに近いな。")
+  }
+
+  def test3 = Action {
+    Ok("session store").withSession(
+      "sample-key" -> "sample-value"
+    )
+  }
+
+  // http://dev.classmethod.jp/server-side/play20-extcontroller/
+  def test4 = Action {
+    implicit request => {
+      session.get("sample-key").map { value =>
+        Ok(value)
+      }.getOrElse {
+        Ok("が、ダメっ!")
+      }
+    }
+  }
+
+  def getSubdomain(domain: String) = {
+    domain.substring(0, domain.indexOf('.'))
+  }
 }
--- app/controllers/Signup.scala
+++ app/controllers/Signup.scala
@@ -7,4 +7,13 @@ object Signup extends Controller {
   def index = Action {
     Ok(views.html.signup("Hello, this is signup!"))
   }
+
+  def indexPost = Action {
+    implicit request => {
+      // println(request.headers)
+      println(request.body.asFormUrlEncoded.get("uname"))
+      println(request.body.asFormUrlEncoded.get("uname").toString())
+      Ok("Postされた")
+    }
+  }
 }
