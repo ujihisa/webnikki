f1991450553eb5663eb8db561e1fad95064e04ed
mahata <nori@mahata.net>
2013-01-17 23:34:12 -0800

    in the middle of implementing signup.

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,13 +1,20 @@
 $(function() {
-    $('#uname').bind('blur', function() {
-        $('#uname-info').html('<img src="/assets/images/loading.gif" />');
-        $.getJSON('/api/exist/uname/' + $(this).val().trim(), function(json) {
-            if (json.exist) {
-                $('#uname-info').html('そのユーザー名は既に使われています。');
-            } else {
-                $('#uname-info').html('');
-            }
+    var errorMessages = {
+        'uname': 'そのユーザー名は既に使われています。',
+        'email': 'そのメールアドレスは既に使われています。'
+    };
+
+    _.each(['uname', 'email'], function(key) {
+        var inputId = '#' + key;
+        var spanId = '#' + key + '-info';
+
+        $(inputId).bind('blur', function() {
+            $(spanId).html('<img src="/assets/images/loading.gif" />');
+            $.getJSON('/api/exist/' + key + '/' + $(this).val().trim(), function(json) {
+                $(spanId).html(json.exist ? errorMessages[key] : '');
+            });
         });
+
     });
 
     $('#password-check').change(function() {
