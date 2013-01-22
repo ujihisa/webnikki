7940d8f0835a844ee2cf73c7d48bf4b5a0cdcca9
mahata <nori@mahata.net>
2013-01-21 19:46:08 -0800

    in the middle of implementing login.

--- app/controllers/Login.scala
+++ app/controllers/Login.scala
@@ -2,9 +2,22 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import views._
+// import models.Member
 
 object LoginController extends Controller {
+  val memberForm = Form(
+    tuple(
+      "email" -> email,
+      "password" -> nonEmptyText(minLength = 4)
+      ))
+
   def index = Action {
-    Ok(views.html.login("Hello, this is login!"))
+    Ok(views.html.login(memberForm, None))
   }
+
+  def indexPost = TODO
 }
