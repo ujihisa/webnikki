619d09adb3b48657da3b6884115409b22638c9fc
mahata <nori@mahata.net>
2013-02-07 03:02:01 -0800

    in the middle of re-implementing submit.

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,4 +1,15 @@
 $(function() {
     console.debug('Hello, this is test.js');
+
+    $('#button').click(function() {
+        console.debug('やふう');
+
+        $().toastmessage('showToast', {
+            text: 'Some information for you ...',
+            type: 'success',
+            close: function() { alert('Hello'); }
+        });
+
+    });
 });
 
