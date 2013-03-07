ed1278e5857d6c08dd06e0552edd65a7498996f7
Yasunori Mahata <nori@mahata.net>
2013-03-06 17:46:25 -0800

    tmp.

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,12 +1,13 @@
 $(function() {
     var Comment = Backbone.Model.extend({
         defaults: {
+			uname: 'no-name',
             content: '',
             created_at: 0
-        },
-        initialize: function() {
-            console.debug('initializing Comment...');
-        }
+        }// ,
+        // initialize: function() {
+        //     console.debug('initializing Comment...');
+        // }
     });
 
     var CommentList = Backbone.Collection.extend({
@@ -19,13 +20,12 @@ $(function() {
     var CommentView = Backbone.View.extend({
         tagName: 'div',
         template: _.template($('#comment-template').html()),
-        initialize: function() {
-            console.log('initializing CommentView');
-        },
+        // initialize: function() {
+        //     console.log('initializing CommentView');
+        // },
         render: function() {
             var data = this.model.toJSON();
-            var html = this.template({someData: 'Hello'});
-            console.debug('data on CommentView', data);
+            var html = this.template({someData: data});
 
             $(this.el).html(html);
             console.debug('rendering in CommentView', $(this.el), html);
@@ -39,33 +39,26 @@ $(function() {
         },
         initialize: function() {
             this.model.bind('add', this.render, this); // ?? don't know about last this
-            console.debug('CommentListView initialize', this.model);
+            // console.debug('CommentListView initialize', this.model);
         },
         render: function() {
-            console.debug('rendering should be written here...');
             var commentListEl = $('#comment-list');
             commentListEl.empty();
 
             this.model.each(function(comment) {
                 var view = new CommentView({model: comment});
                 view.render();
-                console.debug('view.render', view, view.render);
                 commentListEl.append(view.el);
             });
         },
         _onAddInputClick: function() {
-            var comment = new Comment({comment: $('#comment').val()});
+            var comment = new Comment({content: $('#comment').val()});
             $('#comment').val('');
 
             this.model.add(comment);
-            console.debug('Hey, yo-', this.model, this.model.each);
         }
     });
 
-    // var commentListView = new CommentListView({ model: new CommentList() });
     new CommentListView({ model: new CommentList() });
-    // $('#comment-button').click(function() {
-    //     var commnet = new Comment({comment: $('#comment').val()});
-    // });
 });
 
