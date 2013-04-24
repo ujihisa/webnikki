d54456b261c25b2c79f0348b541627f7dad392d1
mahata <nori@mahata.net>
2013-04-23 23:22:25 -0700

    trying to implement RSS

--- app/controllers/RssController.scala
+++ app/controllers/RssController.scala
@@ -1,13 +1,27 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
-import play.api.data._
-import play.api.data.Forms._
-import play.api.libs.json.Json
+//import play.api._
+import play.api.mvc.{Action, Controller}
+import library.Util
+import models.Member
+import models.Post
 
 object RssController extends Controller {
-  def rss(name: String) = Action {
-    Ok("Hello, this should be RSS feed, actually. " + name)
+  def rss = Action {
+    implicit request => {
+      val uname = Util.getUnameFromSubdomain(request.domain)
+      val memberId = Member.selectByUname(uname).get.id
+      val posts = Post.postsByMemberId(memberId)
+      // println(posts)
+      val rss = 
+        <rss version="2.0">
+          <channel>
+            <title>{uname} のRSS</title>
+            <link></link>
+          </channel>
+        </rss>
+
+      Ok("<dummy>Trying to </dummy>").as("application/rss+xml")
+    }
   }
 }
