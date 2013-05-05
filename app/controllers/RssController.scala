bbaccf4c4c9420c03dfb291995d108ff8da923a7
mahata <nori@mahata.net>
2013-05-05 11:44:16 -0700

    in the middle of implementing RSS.

--- app/controllers/RssController.scala
+++ app/controllers/RssController.scala
@@ -5,6 +5,7 @@ import play.api.mvc.{Action, Controller}
 import library.Util
 import models.Member
 import models.Post
+import com.tristanhunt.knockoff.DefaultDiscounter.{knockoff, toXHTML}
 
 object RssController extends Controller {
   def rss = Action {
@@ -12,8 +13,7 @@ object RssController extends Controller {
       val uname = Util.getUnameFromSubdomain(request.domain)
       val memberId = Member.selectByUname(uname).get.id
       val posts = Post.postsByMemberId(memberId)
-      // println(posts)
-      val rss = 
+      val rss =
         <rss version="2.0">
           <channel>
             <title>{uname} のRSS</title>
@@ -30,18 +30,24 @@ object RssController extends Controller {
           { posts map {
               p =>
               <item>
-                <title>title</title>
+                <title>{p.title}</title>
                 <link>https://twitter.com/mahata</link>
-                <description>Hello, world!</description>
+                <description>
+                { "__CDATA_START__" }
+                { toXHTML(knockoff(p.content)) }
+                { "__CDATA_END__" }
+                </description>
                 <pubData>2013-11-14</pubData>
               </item>
             }
           }
         </rss>
 
-      // Ok("<dummy>Trying to </dummy>").as("application/rss+xml")
-      // Ok("<dummy>Trying to </dummy>")
-      Ok(rss)
+      Ok(
+        rss.toString.
+        replace("__CDATA_START__", "<![CDATA[").
+        replace("__CDATA_END__", "]]>")
+      ).as("application/rss+xml")
     }
   }
 }
